5903567ad05bb31948c67a2b52ac9a13fc53645e
Anish Acharya <aanishacharyaa@gmail.com>
2023-08-11 20:59:16 -0400

    added Integration tests

---
+++ wwwroot/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CalculatorAA.wwwroot
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configure services here
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // Configure middleware pipeline here
+        }
+    }
+}
