af2bc56709f6a586980aa5ba03fb8416ead810b8
Anish Acharya <aanishacharyaa@gmail.com>
2023-08-11 23:20:51 -0400

    worked on migration

---
+++ Migration/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CalculatorAA.Models;
+using System.Collections.Generic;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CalculatorAA.Data
+{
+    public class AppDbContext : DbContext  
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CalculatorModel> CalculatorModels { get; set; }
+    }
+}
